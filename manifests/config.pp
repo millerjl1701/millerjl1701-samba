9021664f0d661954c348a6815d97d1df34c1027e
Jeffrey Miller <millerjl1701@gmail.com>
2017-11-16 15:35:56 -0600

    config.pp: Fixes intendation and white space errors

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,17 +10,17 @@ class samba::config {
 
   concat { "${samba::config_dir}/${samba::config_file}":
     ensure => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    path    => "${samba::config_dir}/${samba::config_file}",
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    path   => "${samba::config_dir}/${samba::config_file}",
   }
   concat::fragment { 'global':
     target  => "${samba::config_dir}/${samba::config_file}",
     order   => '0_global',
     content => template($samba::global_config_template),
   }
-  if ! empty($definitions) {  
+  if ! empty($definitions) {
     concat::fragment { 'shares':
       target  => "${samba::config_dir}/${samba::config_file}",
       order   => 'shares',

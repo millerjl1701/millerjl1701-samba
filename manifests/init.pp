8c3bd29ccd928e9e8a380bb3ab9f7a8b2c34f265
David O'Rourke <david.orourke@gmail.com>
2017-12-24 12:51:27 +0000

    Move data from base class to common.yaml

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,20 +19,20 @@
 # @param shares_template Specifies the template to use to construct the shares concat fragment.
 #
 class samba (
-  Stdlib::Absolutepath       $config_dir             = '/etc/samba',
-  String                     $config_file            = 'smb.conf',
-  Hash                       $global_config          = {},
-  String                     $global_config_template = 'samba/global.erb',
-  String                     $package_ensure         = 'present',
-  String                     $package_name           = 'samba',
-  Boolean                    $service_nmb_enable     = false,
-  Enum['running', 'stopped'] $service_nmb_ensure     = 'stopped',
-  String                     $service_nmb_name       = 'nmb',
-  Boolean                    $service_smb_enable     = true,
-  Enum['running', 'stopped'] $service_smb_ensure     = 'running',
-  String                     $service_smb_name       = 'smb',
-  Hash                       $shares_definitions     = {},
-  String                     $shares_template        = 'samba/shares.erb',
+  Stdlib::Absolutepath       $config_dir,
+  String                     $config_file,
+  Hash                       $global_config,
+  String                     $global_config_template,
+  String                     $package_ensure,
+  String                     $package_name,
+  Boolean                    $service_nmb_enable,
+  Enum['running', 'stopped'] $service_nmb_ensure,
+  String                     $service_nmb_name,
+  Boolean                    $service_smb_enable,
+  Enum['running', 'stopped'] $service_smb_ensure,
+  String                     $service_smb_name,
+  Hash                       $shares_definitions,
+  String                     $shares_template,
   ) {
   case $::operatingsystem {
     'RedHat', 'CentOS': {
